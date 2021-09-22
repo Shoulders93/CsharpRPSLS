d47344ad84744774adc839b4a2d844a88809dfd8
Kory Attleson <kory.attleson@gmail.com>
2021-09-22 11:34:18 -0500

    Created rpsls project. Pushing to main repo

---
+++ CsharpRPSLS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpRPSLS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
